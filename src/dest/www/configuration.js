95351fa74b05ac6f7d3e356d2a9a80fa4da6ee4b
Ricardo Padilha <ricardospadilha@gmail.com>
2015-06-25 13:56:33 -0700

    Updated to SDK 2.1, openssl 1.0.2c, sqlite 3.8.10.02, new crosscompile.sh

--- src/dest/www/configuration.js
+++ src/dest/www/configuration.js
@@ -1,12 +1,19 @@
 angular
 .module('webui.services.configuration',  [])
-.constant('$name', 'webui-aria2')
+.constant('$name', 'Aria2 WebUI')
+.constant('$titlePattern', 'active: {active} - waiting: {waiting} - stopped: {stopped} — {name}')
 .constant('$pageSize', 11)
 .constant('$authconf', {
   host: 'localhost',
   port: 6800,
   encrypt: false,
-  auth: {}
+  auth: {
+  // token: '$YOUR_SECRET_TOKEN$'
+  /*-----------------------------*/
+  // user: '*YOUR_USERNAME*',
+  // pass: '*YOUR_SECRET_PASS*'
+  },
+  directURL: ''
 })
 .constant('$enable', {
   torrent: true,
@@ -15,16 +22,14 @@ angular
     show: true,
     stats: true,
     filters: true,
-    starredProps: true,
+    starredProps: true
   }
 })
 .constant('$starredProps', [
-  'dir', 'max-concurrent-downloads', 'max-connection-per-server', 'max-overall-download-limit', 'max-overall-upload-limit', 'conf-path',
+  'dir', 'max-concurrent-downloads', 'max-connection-per-server', 'max-overall-download-limit', 'max-overall-upload-limit', 'conf-path'
 ])
 .constant('$downloadProps', [
   'dir', 'pause', 'max-download-limit', 'max-upload-limit', 'max-connection-per-server'
 ])
 .constant('$globalTimeout', 1000)
 ;
-
-
